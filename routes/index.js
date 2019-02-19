8dc57a5b39ccc4c3389c5947e04eb78cdd0def1f
mdamian9 <m.damian9@hotmail.com>
2019-02-19 11:59:16 -0800

    Setting up test routes / firebase

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.get('/home', (req, res) => {
+  res.send('Home');
+});
+
+router.get("/test", (req, res) => {
+  res.send("Test");
 });
 
 module.exports = router;
